a77f88ef198ac66fe46d6023a3fe113ecd34b765
Aini Nur Fadilah <aininurfadillah2@gmail.com>
2022-12-21 04:49:08 +0700

    feaini

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import "./Login.css"
+
+
+function Login (){
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+
+return (
+    <>
+        <img className="wave" src="/wave.png" alt= ""/>
+        <div class="container">
+            <div className="img">
+                <img src="/login.png" alt=""/>
+            </div>
+            <div className="login-container">
+                <form action="/" method="post">
+                    <h2 className="title">LOG IN</h2>
+                    <div className="input-div one focus">
+                    <div className="i">
+                            <i className="fas fa-user"></i>
+                    </div>
+                    <div className="div">
+                            <h5>email</h5>
+                            <input name="username" className="input-fields" placeholder="Your email" type="email" required />
+                    </div>
+                    </div>
+                    <div className="input-div pass focus">
+                    <div className="i"> 
+                            <i className="fas fa-lock"></i>
+                    </div>
+                    <div className="div">
+                            <h5>Password</h5>
+                            <input name="password" className="input-fields" placeholder="Your Password" type="password" required />
+                    </div>
+                    </div>
+                    <input type="submit" className="btn" value="Login"/>
+                    <a href="/login-admin">Login Sebagai Admin?</a>
+                </form>
+            </div>
+
+        </div>
+    </>
+);
+
+};
+ 
+export default Login;
